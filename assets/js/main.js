fa51ae215547f1f979cc5fb1069bd6db8b83199c
Cezary Wojtkowski <cezary@gmail.com>
2018-05-24 15:26:14 -0700

    style changes to header menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ const menuEl = document.querySelector('.menu');
 const menuToggleEl = document.querySelector('.menu-toggle');
 
 window.addEventListener('click', (e) => {
-  console.log(e.target, e);
   if (menuToggleEl.contains(e.target)) {
     menuEl.classList.toggle('dn');
   } else {
