03dc19d66904eb4ee90aa3608f556f6150ed39b0
Cezary Wojtkowski <cezary@gmail.com>
2018-06-13 20:42:53 -0700

    animate menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@ const menuToggleEl = document.querySelector('.menu-toggle');
 
 window.addEventListener('click', (e) => {
   if (menuToggleEl.contains(e.target)) {
-    menuEl.classList.toggle('dn');
+    menuEl.classList.toggle('menu--open');
   } else {
-    menuEl.classList.toggle('dn', true);
+    menuEl.classList.toggle('menu--open', false);
   }
 });
